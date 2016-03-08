c3fb71f998ccb43865eadd9dac9e1c22a28c8c3f
Tim Visée <timvisee@gmail.com>
2016-03-08 16:26:38 +0100

    Changed version number to 1.0 (9)

--- src/main/java/com/timvisee/glowstonelanterns/GlowstoneLanterns.java
+++ src/main/java/com/timvisee/glowstonelanterns/GlowstoneLanterns.java
@@ -33,12 +33,12 @@ public class GlowstoneLanterns extends JavaPlugin {
     /**
      * The version name of the plugin.
      */
-    private static final String PLUGIN_VERSION_NAME = "0.8";
+    private static final String PLUGIN_VERSION_NAME = "1.0";
 
     /**
      * The version code of the plugin.
      */
-    private static final int PLUGIN_VERSION_CODE = 8;
+    private static final int PLUGIN_VERSION_CODE = 9;
 
     /**
      * The plugin instance.
